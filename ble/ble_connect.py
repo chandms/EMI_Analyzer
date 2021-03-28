c7e4e64eb88509a46b95e767bf1d091f0f3cdccd
tame0001 <thirawat.tam@gmail.com>
2021-03-28 14:50:18 -0400

    Recieve data from ble

--- ble/ble_connect.py
+++ ble/ble_connect.py
@@ -1,10 +1,41 @@
 import asyncio
+import array
 from bleak import BleakScanner
+from bleak import discover
+from bleak import BleakClient
 
-async def run():
-    devices = await BleakScanner.discover()
-    for d in devices:
-        print(d)
+ADDRESS = 'C6:3F:7F:8E:A7:D1'
+UART_NORDIC = '6E400001-B5A3-F393-E0A9-E50E24DCCA9E'
+UUID_NORDIC_RX = '6E400003-B5A3-F393-E0A9-E50E24DCCA9E'
+UUID_NORDIC_TX = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E'
+
+command = 'Do you hear me?'
+
+# async def run():
+#     devices = await BleakScanner.discover()
+#     for d in devices:
+#         print(d)
+
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(run())
+
+def uart_data_received(sender, data):
+    print(f'RX> {data}')
+
+print('Connecting...')
+async def run(ADDRESS, loop):
+    async with BleakClient(ADDRESS, loop=loop) as client:
+        print('Connected')
+        await client.start_notify(UUID_NORDIC_RX, uart_data_received)
+        # print('Writing command')
+        # c=command
+        # while len(c)>0:
+        #   await client.write_gatt_char(UUID_NORDIC_TX, bytearray(c[0:10]), True)
+        #   c = c[20:]
+        print('Waiting for data')
+        await asyncio.sleep(5.0, loop=loop) # wait for a response
+        print('Done!')
+
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(run())
+loop.run_until_complete(run(ADDRESS, loop))
