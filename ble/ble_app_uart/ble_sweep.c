43d79cf22979a08125f7fa00a82119ac5d66308a
tame0001 <thirawat.tam@gmail.com>
2021-04-20 21:06:53 -0400

    move includes and defines to separeted files

---
+++ ble/ble_app_uart/ble_sweep.c
@@ -0,0 +1,11 @@
+/*
+Author: Thirawat Bureetes
+Email: tbureete@purdue.edu
+Date: 4/20/2021
+Description: A file contains fucntions and parameters for transfering sweep file via ble.
+*/
+
+
+#include "sweep.h"
+#include "ble_sweep.h"
+
