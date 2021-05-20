e623043550bb7e943a8fb5e1b08d0c462812bc34
tame0001 <thirawat.tam@gmail.com>
2021-05-20 21:29:34 +0000

    chanage restx to restful

--- server/backend/app.py
+++ server/backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
-from flask_restx import Api
+from flask_restful  import Api
+from flask_cors import CORS
 from upload import UploadHandler
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 api.add_resource(UploadHandler, '/upload/')
 
--- server/backend/upload.py
+++ server/backend/upload.py
@@ -1,4 +1,4 @@
-from flask_restx import Resource, reqparse
+from flask_restful  import Resource, reqparse
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import FileStorage
 from pathlib import Path
@@ -7,7 +7,10 @@ path = Path('/home/tam/git/EMI/sweeps')
 
 class UploadHandler(Resource):
 
-      def post(self):
+    def get(self):
+        return {'message': 'If you see this, it means the system works!'}
+
+    def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('file', type=FileStorage, location='files', required=True)
         args = parser.parse_args()
