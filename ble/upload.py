e4c9fb3f97d588c61edab4f7d091a3fd0e14ab17
tame0001 <thirawat.tam@gmail.com>
2021-05-21 18:06:34 -0400

    create config file

--- ble/upload.py
+++ ble/upload.py
@@ -1,23 +1,27 @@
 import requests
+import yaml
 from pathlib import Path
 from random import choice
 
-url = 'http://10.165.76.237:50000/upload/'
-
-def upload_sweep(sweep):
+def upload_sweep(sweep, uri):
     with open(sweep, 'rb') as f:
         files = {'file': f}
-        r = requests.post(url, files=files)
+        r = requests.post(uri, files=files)
         return r
+
 
 if __name__ == '__main__':
 
-    path = Path('/home/pi/Desktop/EMI/sweeps')
+    with open('config.yaml') as f:
+        configs = yaml.load(f, Loader=yaml.FullLoader)
+    
+    path = Path(configs['sweep_path'])
     sweeps = []
     for x in path.iterdir():
         sweeps.append(x)
-
     sweep = choice(sweeps)
-    r = upload_sweep(sweep)
-    print(r.status_code)
+    uri = configs['upload_uri'][configs['env']]
+    print(f'Uploading sweep file to {uri}')
+    r = upload_sweep(sweep, uri)
+    print(f'Status code = {r.status_code}')
     print(r.text)
