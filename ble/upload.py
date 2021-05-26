0a53edbace8069d7b9cdd5af7490c8b4a0032d24
tame0001 <thirawat.tam@gmail.com>
2021-05-26 13:56:26 -0400

    add sweep metadata along with upload file

--- ble/upload.py
+++ ble/upload.py
@@ -1,12 +1,18 @@
 import requests
 import yaml
+import re
 from pathlib import Path
 from random import choice
 
 def upload_sweep(sweep, uri):
+    group = re.findall(r'(.*)-(\d{4}-\d{2}-\d{2}T.{8})', sweep.stem)[0]
+    body = {
+        'device_name': group[0],
+        'hub_time': group[1]
+    }
     with open(sweep, 'rb') as f:
         files = {'file': f}
-        r = requests.post(uri, files=files)
+        r = requests.post(uri, files=files, data=body)
         return r
 
 
