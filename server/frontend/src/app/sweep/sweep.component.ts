eb2f0115c91a74bb8a67f3c6906585a81ac84341
tame0001 <thirawat.tam@gmail.com>
2021-06-13 19:24:07 -0400

    able to select all or latest sweep

--- server/frontend/src/app/sweep/sweep.component.ts
+++ server/frontend/src/app/sweep/sweep.component.ts
@@ -15,15 +15,29 @@ import { saveAs } from 'file-saver';
   styleUrls: ['./sweep.component.css']
 })
 export class SweepComponent implements OnInit {
-  sweeps :any;
+  sweeps: Sweep[] = [];
+  showAllSweeps: boolean = false;
+  allSweep: Sweep[] = [];
+  latestSweep: Sweep[] = [];
+
 
   constructor(private service: SweepService) {}
 
   ngOnInit() {
-    this.service.getAllSweeps()
+    this.service.getLatestSweep()
       .subscribe(Response => {
-        this.sweeps = Response;
-        this.sweeps.forEach((sweep: Sweep) => {
+        this.latestSweep = Response;
+        this.latestSweep.forEach((sweep: Sweep) => {
+          sweep.hub_timestamp = new Date(sweep.hub_timestamp);
+          sweep.server_timestamp = new Date(sweep.server_timestamp);
+          this.sweeps = this.latestSweep;
+        });
+      })
+
+    this.service.getAllSweeps()    
+      .subscribe(Response => {
+        this.allSweep = Response;
+        this.latestSweep.forEach((sweep: Sweep) => {
           sweep.hub_timestamp = new Date(sweep.hub_timestamp);
           sweep.server_timestamp = new Date(sweep.server_timestamp);
         });
@@ -36,6 +50,16 @@ export class SweepComponent implements OnInit {
       saveAs(Response, sweep.filename);
     });
   }
+
+  sweepDisplayChange() {
+    if (this.showAllSweeps) {
+      this.sweeps = this.allSweep;
+    }
+    else {
+      this.sweeps = this.latestSweep;
+    }
+  }
+
 }
 
 export interface Sweep {
