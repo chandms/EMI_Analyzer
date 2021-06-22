d14f84ed2e3cd28455d84f178c5fcf4d624938b4
tame0001 <thirawat.tam@gmail.com>
2021-06-22 19:33:25 -0400

    add router page

--- server/frontend/src/app/app.module.ts
+++ server/frontend/src/app/app.module.ts
@@ -2,24 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SweepComponent } from './sweep/sweep.component';
 import { SweepService } from './sweep/sweep.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DeviceComponent } from './device/device.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SweepComponent
+    SweepComponent,
+    DeviceComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: SweepComponent
+      },
+      {
+        path: 'device',
+        component: DeviceComponent
+      }
+    ])
   ],
   providers: [
     SweepService
