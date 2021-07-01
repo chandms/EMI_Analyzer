8bf65727e67f222210dd929b9a7927877e5237f3
tame0001 <thirawat.tam@gmail.com>
2021-06-30 20:03:22 -0400

    update center and marker location

--- server/frontend/src/app/location/location.component.ts
+++ server/frontend/src/app/location/location.component.ts
@@ -7,7 +7,6 @@ import TileLayer from 'ol/layer/Tile';
 import View from 'ol/View';
 import Feature from 'ol/Feature';
 import { Point } from 'ol/geom';
-import { Icon, Style } from 'ol/style';
 import { fromLonLat } from 'ol/proj';
 import { Vector } from 'ol/layer';
 
@@ -23,45 +22,39 @@ export class LocationComponent implements OnInit {
   device_name: string = '';
   latitude: number = 40.421488;
   longitude: number = -86.916631;
+  
+  marker = new Feature({
+    geometry: new Point(fromLonLat([this.longitude, this.latitude]))
+  });
+
+  layer = new Vector({
+    source: new olsource.Vector({features: [this.marker]})
+  });
 
   map!: Map;
-  layer!: Vector;
+  zoom: number = 15;
 
   ngOnInit(): void {
     this.device_name = this.route.snapshot.params.device_name;
+    let position = fromLonLat([this.longitude, this.latitude]);
     this.map = new Map({
       target: 'device_map',
       layers: [
-        new TileLayer({
-          source: new olsource.OSM()
-        })
+        new TileLayer({source: new olsource.OSM()})
       ],
       view: new View({
-        center: fromLonLat([
-          this.longitude, 
-          this.latitude
-        ]),
-        zoom: 15
+        center: position,
+        zoom: this.zoom
       })
     });
-
-    this.layer = new Vector({
-      source: new olsource.Vector({
-        features: [
-          new Feature({
-            geometry: new Point(fromLonLat([
-              this.longitude, 
-              this.latitude
-            ]))
-          })
-        ]
-      })
-    });
+    
     this.map.addLayer(this.layer);
+
   }
 
   changeLocation() {
-    console.log("Latitude = " + this.latitude);
-    console.log("Longitude = " + this.longitude);
+    let new_position = fromLonLat([this.longitude, this.latitude]);
+    this.map.getView().setCenter(new_position);
+    this.marker.setGeometry(new Point(new_position));
   }
 }
